7aa4750ced7f42a44049f5e872dbc10a376c4892
Sergei O. Udalov <udalov.x@mail.ru>
2024-06-11 08:16:21 +0300

    init prototype

---
+++ test/first_spec.rb
@@ -0,0 +1,9 @@
+TestingFramework.describe 'Test case' do
+  it { expect(1).to eq 1 }
+  it { expect(1).to eq 2 }
+  it { expect(1).to eq 1 }
+  it { expect(1).to eq 1 }
+  it { expect(1).to eq 1 }
+  it { expect(1).to eq 1 }
+  it { expect(1).to eq 7 }
+end
