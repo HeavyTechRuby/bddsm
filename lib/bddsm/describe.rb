da70400882d2a73e8a4aae3ea6f2173e321cb0ff
lain0 <trampampushe4ka@gmail.com>
2024-06-12 04:25:25 +0300

    feat: #7 add support for let 2

--- lib/bddsm/describe.rb
+++ lib/bddsm/describe.rb
@@ -17,7 +17,9 @@ module BDDSM
     end
 
     def let(name, &)
+      self.singleton_class.instance_eval { attr_accessor name.to_sym }
       @memoized_lets[name] ||= instance_exec(&)
+      self.instance_variable_set("@#{name}", instance_exec(&))
     end
   end
 end
---
+++ test/let2_spec.rb
@@ -0,0 +1,5 @@
+BDDSM.describe 'Some' do
+  let(:one) { 1 }
+  let(:two) { one + one }
+  it { expect(two).to eq 2 }
+end
