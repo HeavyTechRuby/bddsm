791d7a8e840d68956ff4f801799497bd1d1fc32d
lain0 <trampampushe4ka@gmail.com>
2024-06-12 10:52:08 +0300

    refactor: #7 fix overriding let

--- test/let2_spec.rb
+++
@@ -1,5 +0,0 @@
-BDDSM.describe 'Some' do
-  let(:one) { 1 }
-  let(:two) { one + one }
-  it { expect(two).to eq 2 }
-end
--- test/let_spec.rb
+++ test/let_spec.rb
@@ -1,4 +1,15 @@
-BDDSM.describe 'Some' do
+BDDSM.describe 'BDDSM::Describe.let' do
+  # let should define variable accessible from 'it'
   let(:a) { 1 }
   it { expect(a).to eq 1 }
+
+  # let variable should be accessible from another let
+  let(:one) { 1 }
+  let(:two) { one + one }
+  it { expect(two).to eq 2 }
+
+  # let variable should be overriden
+  let(:three) { 3 }
+  let(:three) { 4 }
+  it { expect(three).to eq 4 }
 end
