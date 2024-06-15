17e98d39f2c25ecff23a8b6f6b11316dccdde5e2
lain0 <trampampushe4ka@gmail.com>
2024-06-16 01:06:31 +0300

    style: #12 remove frozen_string_literal strings

--- lib/bddsm/equal_matcher.rb
+++ lib/bddsm/equal_matcher.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module BDDSM
   class EqualMatcher
     def initialize(expected:, location:)
--- lib/bddsm/progress_report.rb
+++ lib/bddsm/progress_report.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module BDDSM
   class ProgressReport
     def initialize(result:, io:)
