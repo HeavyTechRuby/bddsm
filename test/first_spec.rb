be25ab576c33112986b053ec2329f6347ebfe69f
Sergei O. Udalov <udalov.x@mail.ru>
2024-07-13 14:03:24 +0300

    correct exit code on exit

--- test/first_spec.rb
+++ test/first_spec.rb
@@ -1,9 +1,4 @@
 BDDSM.describe 'Test case' do
   it { expect(1).to eq 1 }
-  it { expect(1).to eq 2 }
   it { expect(1).to eq 1 }
-  it { expect(1).to eq 1 }
-  it { expect(1).to eq 1 }
-  it { expect(1).to eq 1 }
-  it { expect(1).to eq 7 }
 end
