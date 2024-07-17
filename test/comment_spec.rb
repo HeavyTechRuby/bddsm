14e4da08ec105c0b048795d43c5d3a2595553937
fartem <jaman.smlnsk@gmail.com>
2024-07-17 23:41:23 +0300

    updated test cases for comments

--- test/comment_spec.rb
+++ test/comment_spec.rb
@@ -1,7 +1,7 @@
 BDDSM.describe 'Test case with comments' do
   it 'Description example 1' do
-    expect(1).to eq 2
+    expect(1).to eq 1
   end
 
-  it('Description example 2') { expect(1).to eq 3 }
+  it('Description example 2') { expect(1).to eq 1 }
 end
