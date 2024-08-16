580f21ddd9d1b888d28fc60d5d5dc3a8af7ce522
fartem <jaman.smlnsk@gmail.com>
2024-08-16 16:49:57 +0300

    added context block to Describe

---
+++ test/describe_context.rb
@@ -0,0 +1,9 @@
+BDDSM.describe 'Test case with context' do
+  context 'Context example 1' do
+    it 'First it in Context example 1' do
+      expect(1).to eq 1
+    end
+
+    it('Second it in Context example 1') { expect(1).to eq 1 }
+  end
+end
