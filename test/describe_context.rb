15921318162695f30c7b9410df4d9a5803661660
fartem <jaman.smlnsk@gmail.com>
2024-09-28 13:49:34 +0300

    added nested context block tests

--- test/describe_context.rb
+++ test/describe_context.rb
@@ -7,3 +7,17 @@ BDDSM.describe 'Test case with context' do
     it('Second it in Context example 1') { expect(1).to eq 1 }
   end
 end
+
+BDDSM.describe 'Test case with nested context' do
+  context 'Context example 1' do
+    it 'First it in Context example 1' do
+      expect(1).to eq 1
+    end
+
+    context 'Nested Context example 1' do
+      it 'First it in nested Context example 1' do
+        expect(1).to eq 1
+      end
+    end
+  end
+end
